15b04236af6e8a22de9de69192e468bcaa5a3594
Greshniksv <greshnik-sv@yandex.ru>
2014-05-28 00:59:43 +0300

    Start work with variable system

--- GRInstaller3000Classes/ScriptEngine.cs
+++ GRInstaller3000Classes/ScriptEngine.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Windows.Forms;
 
@@ -20,8 +22,17 @@ namespace GRInstaller3000Classes
 	        }
         }
 
-        private List<FunctionItem> _functionList;
-	    private Commands _commands;
+
+		class VariableItem
+	    {
+		    public string Name { get; set; }
+			public object Data { get; set; }
+			public string Type { get; set; }
+	    }
+
+        private readonly List<FunctionItem> _functionList;
+	    private readonly Commands _commands;
+	    private Hashtable _variableList; 
 
         public Commands Command
         {
@@ -32,21 +43,76 @@ namespace GRInstaller3000Classes
         {
 			_commands = new Commands();
             _functionList = new List<FunctionItem>();
+			_variableList = new Hashtable();
         }
 
 	    public void ExecuteFunc(string funcName="main")
 	    {
 		    var managerialWords = new List<string>(){"if","else","end", "while"};
+			var variableTypeList = Enum.GetNames(typeof(VariableType));
 		    var commandList = _commands.GetCommandList();
 		    var func = _functionList.FirstOrDefault(i => i.Name == funcName);
 			if(func==null) throw new Exception("Function ["+funcName+"] not found !");
 
 			foreach (var codeItem in func.Code)
 			{
-				bool isManagerialWords = managerialWords.Where(managerialWord => codeItem.Contains(managerialWord)).Any();
+				var newVar = variableTypeList.FirstOrDefault(i => codeItem.Contains(i));
+
+				bool isManagerialWords = managerialWords.Any(managerialWord => codeItem.Contains(managerialWord));
 				if (!isManagerialWords) _commands.Execute(codeItem);
 		    }
 
+	    }
+
+	    public void CreateVariable(string data)
+	    {
+		    if (data.Contains("="))
+		    {
+				// contains initializer
+				var dataMass = data.Split(' ','=');
+			    if (dataMass.Count() != 3)  throw new Exception("Variable with initializer structure error !");
+			    object variable = null;
+
+			    if (string.Equals(dataMass[0], "String", StringComparison.CurrentCultureIgnoreCase))
+				    variable = dataMass[3];
+
+			    if (string.Equals(dataMass[0], "Int", StringComparison.CurrentCultureIgnoreCase))
+				    variable = int.Parse(dataMass[3]);
+
+			    if (string.Equals(dataMass[0], "Bool", StringComparison.CurrentCultureIgnoreCase))
+				    variable = Boolean.Parse(dataMass[3]);
+
+				if (string.Equals(dataMass[0], "Double", StringComparison.CurrentCultureIgnoreCase))
+					variable = Double.Parse(dataMass[3]);
+
+				// Example ListString Bob = {"1","1","1","1","1"}
+			    if (string.Equals(dataMass[0], "ListString", StringComparison.CurrentCultureIgnoreCase))
+			    {
+				    if (!dataMass[3].Contains('{') || !dataMass[3].Contains('}'))
+						throw new Exception("ListString initializer incorrect !");
+
+				    var r = dataMass[3].Split('s');
+				    
+
+					variable = dataMass[3].Replace('{', ' ').Replace('}', ' ').Replace('"', ' ').Trim().Split(',').ToList();
+			    }
+			    //variable = string.Empty;
+
+				if (string.Equals(dataMass[0], "ListInt", StringComparison.CurrentCultureIgnoreCase))
+					variable = string.Empty;
+
+				//dataMass: 0 - type, 1 - name, 2 - data
+				_variableList.Add(dataMass[1], new VariableItem() { Name = dataMass[1], Type = dataMass[0], Data = variable });
+		    }
+		    else
+		    {
+			    
+		    }
+
+		    
+
+
+			
 	    }
 
 
